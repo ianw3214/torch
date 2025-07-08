616f57b9f93b2c9d433e92fd602a0fa17011d7a9
ianw3214 <ianw3214@gmail.com>
2025-07-08 00:44:06 -0700

    Recursive folder/file listing
    Display files and folders based on folder depth

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,39 @@ slint::include_modules!();
 
 use slint::{ModelRc, VecModel};
 
+fn build_folder_items(path : &std::path::Path, depth : i32) -> Vec<FolderItem> {
+    let mut items = Vec::new();
+
+    if let Ok(entries) = std::fs::read_dir(path) {
+        let mut entries = entries.filter_map(Result::ok).collect::<Vec<_>>();
+        entries.sort_by_key(|e| e.file_name());
+        for entry in entries {
+            let path = entry.path();
+            let is_folder = path.is_dir();
+            items.push(FolderItem {
+                // TODO: using to_string_lossy might result in less errors
+                name: entry.file_name().into_string().unwrap().into(),
+                depth,
+                is_folder,
+                is_expanded : false
+            });
+            
+            if is_folder {
+                items.extend(build_folder_items(&path, depth + 1));
+            }
+        }
+    }
+
+    items
+}
+
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = MainWindow::new()?;
 
-    let path = "./";
-    let folder_names = std::fs::read_dir(path)
-        .unwrap()
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.path().is_dir())
-        .filter_map(|e| e.file_name().into_string().ok())
-        .collect::<Vec<_>>();
-
-    // Need to convert rust strings to slint usable strings
-    let folder_names : Vec<slint::SharedString> = folder_names.into_iter().map(Into::into).collect();
-    // A modelRc is a reference counted wrapper to a model that allows rust to provide dynamic lists to slint
-    let model = ModelRc::new(VecModel::from(folder_names));
+    // TODO: Replace path w/ project path
+    let path = std::path::Path::new("./");
+    let items = build_folder_items(path, 0);
+    let model = ModelRc::new(VecModel::from(items));
     main_window.set_folders(model);
 
     main_window.run()
